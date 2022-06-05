2eb5fd982a750da195aa5f03083316051ef80e61
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-05 03:25:01 -0300

    Inserindo parametros de paginação

--- src/main/java/com/api/project/blog/controller/PostController.java
+++ src/main/java/com/api/project/blog/controller/PostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,11 +31,13 @@ public class PostController {
 	public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
 		return new ResponseEntity<PostDto>(
 				 postService.createPost(postDto)
-				,HttpStatus.CREATED);
+				,HttpStatus.CREATED); 
 	}
 	
 	@GetMapping
-	public List<PostDto> getAllPosts() {
+	public List<PostDto> getAllPosts(@RequestParam(value = "pageNo", defaultValue = "0",required = false)int pageNo,
+			                         @RequestParam(value = "pageSize", defaultValue = "10")int pageSize 
+			) {
 		return postService.getAllPosts();
 	}
 	
