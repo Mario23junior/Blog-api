d91f671595102c9509f5e844aef7f30d35eb3deb
Mário Junior <48899365+Mario23junior@users.noreply.github.com>
2022-06-10 03:30:19 -0300

    remodelando conversao de entidades

--- src/main/java/com/api/project/blog/service/impl/PostService.java
+++ src/main/java/com/api/project/blog/service/impl/PostService.java
@@ -10,6 +10,7 @@ import com.api.project.blog.payload.PostResponse;
 import com.api.project.blog.repository.PostRepository;
 import com.api.project.blog.service.PostServiceImpl;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -20,9 +21,11 @@ import org.springframework.stereotype.Service;
 public class PostService implements PostServiceImpl{
    
 	private PostRepository postRepository;
+	private ModelMapper mapper;
 	
-    public PostService(PostRepository postRepository) {
+    public PostService(PostRepository postRepository, ModelMapper mapper) {
 		this.postRepository = postRepository;
+		this.mapper = mapper;
 	}
 	
 	@Override
@@ -57,19 +60,12 @@ public class PostService implements PostServiceImpl{
  	}
 
 	private PostDto mapToDTO(Post post) {
-		PostDto postDto = new PostDto();
-		postDto.setId(post.getId());
-		postDto.setTitle(post.getTitle());
-		postDto.setDescription(post.getDescription());
-		postDto.setContent(post.getContent());
+		PostDto postDto = mapper.map(post, PostDto.class);
 		return postDto;
 	}
 	
   	private Post mapToEntity(PostDto postDto) {
-		Post post = new Post();
-		post.setTitle(postDto.getTitle());
-		post.setDescription(postDto.getDescription());
-		post.setContent(postDto.getContent());
+		Post post = mapper.map(postDto, Post.class);
 		return post;
 	}
 
